fac4dad8791654e0975b4c2dda187017d1a7623c
Balázs Láng <balazs.lang@outlook.de>
2021-05-28 17:58:47 +0200

    Make trait handle repeatable plugin

--- plugin-carbon/JSONFieldTrait.php
+++ plugin-carbon/JSONFieldTrait.php
@@ -9,15 +9,23 @@ use Atomino\Carbon\Model;
  */
 trait JSONFieldTrait {
     #[EventHandler(Entity::EVENT_ON_LOAD)]
-    protected function JsonSubFieldTrait_onLoad($event, $data) {
-        $model = JSONField::fetch(static::model());
-        $this->{$model->targetField} = $model->targetClass::fromArray($this->{$model->sourceField});
+    protected function JsonFieldTrait_onLoad($event, $data) {
+        foreach ($this->JsonFieldTrait_fetchPluginList() as $plugin) {
+            $this->{$plugin->targetField} = $plugin->targetClass::fromArray($this->{$plugin->sourceField});
+        }
     }
 
     #[EventHandler(Entity::EVENT_BEFORE_INSERT, Entity::EVENT_BEFORE_UPDATE)]
-    protected function JsonSubFieldTrait_BeforeInsert($event, $data){
-        $model = JSONField::fetch(static::model());
+    protected function JsonFieldTrait_BeforeInsert($event, $data) {
+        foreach ($this->JsonFieldTrait_fetchPluginList() as $plugin) {
+            $this->{$plugin->sourceField} = $this->{$plugin->targetField}->toArray();
+        }
+    }
 
-        $this->{$model->sourceField} = $this->{$model->targetField}->toArray();
+    /**
+     * @return JSONField[]
+     */
+    private function JsonFieldTrait_fetchPluginList(): array {
+        return is_array($plugins = JSONField::fetch(static::model())) ? $plugins : [$plugins];
     }
 }
